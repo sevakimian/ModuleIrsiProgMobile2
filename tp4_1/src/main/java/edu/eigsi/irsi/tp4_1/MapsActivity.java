7fc78451a96f1ae5512eed16e40f59e388af29eb
sevak.hovakimian.19 <sevak.hovakimian.19@gmail.com>
2018-10-22 18:04:55 +0200

    22/10 18h00

--- tp4_1/src/main/java/edu/eigsi/irsi/tp4_1/MapsActivity.java
+++ tp4_1/src/main/java/edu/eigsi/irsi/tp4_1/MapsActivity.java
@@ -1,22 +1,33 @@
 package edu.eigsi.irsi.tp4_1;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private Marker marqueur;
+    private LocationListener Listener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,16 +39,65 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         mapFragment.getMapAsync(this);
     }
 
-
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-        mMap.getUiSettings().setZoomControlsEnabled(true);
+        this.mMap = googleMap;
+        this.mMap.getUiSettings().setZoomControlsEnabled(true);
 
 
         LatLng eigsi = new LatLng(46.139717, -1.154308);
-        mMap.addMarker(new MarkerOptions().position(eigsi).title("Ici c'est l'EIGSI"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(eigsi,17 ));
+        this.mMap.addMarker(new MarkerOptions().position(eigsi).title("Ici c'est l'EIGSI"));
+
+        this.mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(eigsi, 17));
+
+
+
+        LocationManager locManager = (LocationManager) this.getSystemService(this.LOCATION_SERVICE);
+
+        LocationListener locListener = new LocationListener() {
+
+            @Override
+            public void onLocationChanged(Location location) {
+                MapsActivity.this.majPosition(location);
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        if(locManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+
+            locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5, 0, locListener);
+        }
+        else if(locManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)){
+            locManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,5,0,locListener);
+        }
+        else {
+            Toast.makeText(this, "Ca marche pas",Toast.LENGTH_LONG).show();
+        }
+
     }
 
     @Override
@@ -47,8 +107,6 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         return true;
     }
-
-
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -66,16 +124,32 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         return super.onOptionsItemSelected(item);
     }
 
-    public void changeTerrain(GoogleMap mMap){
+    public void changeTerrain(GoogleMap mMap) {
 
         Log.v("terrain", Integer.toString(mMap.getMapType()));
 
-        if(mMap.getMapType() == 2){
+        if (mMap.getMapType() == 2) {
             mMap.setMapType(1);
-            }
-        else{
+        } else {
             mMap.setMapType(2);
         }
 
     }
+
+
+
+
+
+
+
+    public void majPosition(Location location){
+
+        LatLng position = new LatLng(location.getLatitude(), location.getLongitude());
+        if (this.marqueur !=null){
+            this.marqueur.remove();
+        }
+
+        this.marqueur= this.mMap.addMarker(new MarkerOptions().position(position).title("Vous êtes ici"));
+    }
+
 }
